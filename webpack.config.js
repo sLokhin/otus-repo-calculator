92ae8f01278d72ffcef7edb4b913b27d3209208d
sergeyLokhin <lokhin@uchi.ru>
2021-01-16 22:28:32 +0300

    init babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path")
 
 const HtmlWebpackPlugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
     entry: {
@@ -13,11 +14,21 @@ module.exports = {
         path: path.resolve(__dirname, "./dist"),
         filename: "index.js"
     },
+    module: {
+        rules: [
+            {
+                test: /\.(js|ts)x?$/,
+                loader: "babel-loader",
+                exclude: /node_modules/
+            }
+        ]
+    },
     plugins: [
         new HtmlWebpackPlugin({
             title: "Lesson calculator",
             template: path.resolve(__dirname, "./src/template.html"), //шаблон
             filename: "index.html", //название выходного файла
-        })
+        }),
+        new CleanWebpackPlugin(),
     ]
 }
