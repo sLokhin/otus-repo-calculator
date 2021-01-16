295dfa96a6412d5aac0511f181c45745e6dbc21a
sergeyLokhin <lokhin@uchi.ru>
2021-01-17 01:56:22 +0300

    init prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,15 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
-  },
-  'extends': [
-    'plugin:react/recommended',
-    'google',
+  "extends": [
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended"
   ],
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true,
-    },
-    'ecmaVersion': 12,
-    'sourceType': 'module',
+  "rules": {
+    "prettier/prettier": "error"
   },
-  'plugins': [
-    'react',
-    '@typescript-eslint',
-  ],
-  'rules': {
-  },
+  "parserOptions": {
+    "sourceType": "module",
+    "parser": "babel-eslint",
+    "ecmaVersion": 2018
+  }
 };
