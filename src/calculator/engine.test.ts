bd5af5da89351f78934ab7c17fd57ebab25f7995
sergeyLokhin <lokhin@uchi.ru>
2021-01-17 23:35:14 +0300

    calculator operations +, -, *, /

---
+++ src/calculator/engine.test.ts
@@ -0,0 +1,47 @@
+import { firstPrioritiesCalc, secondPrioritiesCalc } from "./engine";
+
+describe("firstPrioritiesCalc simple cases", () => {
+  it("[1, * 26]", () => {
+    expect(firstPrioritiesCalc([1, "*", 26])).toEqual([26]);
+  });
+
+  it("[32, /, 32]", () => {
+    expect(firstPrioritiesCalc([32, "/", 32])).toEqual([1]);
+  });
+
+  it("[26, + 26]", () => {
+    expect(firstPrioritiesCalc([26, "+", 26])).toEqual([26, "+", 26]);
+  });
+});
+
+describe("firstPrioritiesCalc mixed with second priorities cases", () => {
+  it("[32, /, 16, +, 5, *, 20]", () => {
+    expect(firstPrioritiesCalc([32, "/", 16, "+", 5, "*", 20])).toEqual([
+      2,
+      "+",
+      100,
+    ]);
+  });
+});
+
+describe("secondPrioritiesCalc invalid cases", () => {
+  it("[32, / 32]", () => {
+    expect(() => secondPrioritiesCalc([32, "/", 32])).toThrow(
+      TypeError("Unexpected stack!")
+    );
+  });
+});
+
+describe("secondPrioritiesCalc simple cases", () => {
+  it("[32, + 32]", () => {
+    expect(secondPrioritiesCalc([32, "+", 32])).toEqual(64);
+  });
+
+  it("[32, - 32]", () => {
+    expect(secondPrioritiesCalc([32, "-", 32])).toEqual(0);
+  });
+
+  it("[32, - 32, +, 10]", () => {
+    expect(secondPrioritiesCalc([32, "-", 32, "+", 10])).toEqual(10);
+  });
+});
