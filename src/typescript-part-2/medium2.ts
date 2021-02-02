23a2859ccb0ab425e3263aacb192b163d3c1db90
sergeyLokhin <lokhin@uchi.ru>
2021-02-03 00:28:23 +0300

    try medium-2

--- src/typescript-part-2/medium2.ts
+++ src/typescript-part-2/medium2.ts
@@ -2,12 +2,19 @@
 // Есть функция которая достает из реакт компонента (любого, и Functional и Class) его defaultProps
 // Нужно заменить FIXME на правильный тип
 
+import { TestComponent } from "./components/TestComponent";
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-type FIXME = any;
+
+// type FIXME = any;
+
+type FIXME<T> = T extends React.Component<infer Props> ? Props: never;
 
 // Hint: infer
 export const getDefaultProps = <T>(
   component: React.ComponentType<T>
-): FIXME => {
+): FIXME<T> => {
   return component.defaultProps;
 };
+
+console.log("EXECUTE medium-2 :", getDefaultProps(TestComponent))
