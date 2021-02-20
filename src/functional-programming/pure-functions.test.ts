85bab181e9a6611cf5fa83ad8e8887a2b94c5030
sergeyLokhin <lokhin@uchi.ru>
2021-02-20 16:37:06 +0300

    refactor tests

--- src/functional-programming/pure-functions.test.ts
+++ src/functional-programming/pure-functions.test.ts
@@ -1,75 +1,92 @@
 import { Team, getTopName, createQs, parseQs, QsObj } from "./pure-functions";
 
 // Task 1
-test("getTopName test", () => {
-  const teams0 = [] as Team[];
+describe("getTopName test", () => {
+  test("empty mass", () => {
+    const teams0 = [] as Team[];
+    expect(getTopName(teams0)).toBe("Default message");
+  });
 
-  const teams1 = [
-    { name: "Team0", score: 0 },
-    { name: "Team1", score: -1 },
-    { name: "Team2", score: -2 },
-    { name: "Team3", score: -3 },
-  ];
+  test("common mass", () => {
+    const teams1 = [
+      { name: "Team0", score: 0 },
+      { name: "Team1", score: -1 },
+      { name: "Team2", score: -2 },
+      { name: "Team3", score: -3 },
+    ];
+    expect(getTopName(teams1)).toBe("Team0");
+  });
 
-  const teams2 = [
-    { name: "Team0", score: 9 },
-    { name: "Team1", score: 9 },
-    { name: "Team2", score: 9 },
-    { name: "Team3", score: 9 },
-  ];
-
-  expect(getTopName(teams0)).toBe("Default message");
-  expect(getTopName(teams1)).toBe("Team0");
-  expect(getTopName(teams2)).toBe("Team3");
+  test("same score", () => {
+    const teams2 = [
+      { name: "Team0", score: 9 },
+      { name: "Team1", score: 9 },
+      { name: "Team2", score: 9 },
+      { name: "Team3", score: 9 },
+    ];
+    expect(getTopName(teams2)).toBe("Team3");
+  });
 });
 
 // Task 2
-test("createQs test", () => {
-  const qsObj0: QsObj = {
-    page: "2",
-    pageSize: "10",
-    total: "205",
-    somethingElse: "value",
-  };
+describe("createQs test", () => {
+  test("common object", () => {
+    const qsObj0: QsObj = {
+      page: "2",
+      pageSize: "10",
+      total: "205",
+      somethingElse: "value",
+    };
+    expect(createQs(qsObj0)).toBe(
+      "?page=2&pageSize=10&total=205&somethingElse=value"
+    );
+  });
 
-  const qsObj1: QsObj = {
-    page: 11,
-    pageSize: 12,
-    total: 350,
-    somethingElse: "anotherValue",
-  };
+  test("object with numbers as values", () => {
+    const qsObj1: QsObj = {
+      page: 11,
+      pageSize: 12,
+      total: 350,
+      somethingElse: "anotherValue",
+    };
+    expect(createQs(qsObj1)).toBe(
+      "?page=11&pageSize=12&total=350&somethingElse=anotherValue"
+    );
+  });
 
-  const qsObj2: QsObj = {
-    page: 99,
-  };
-
-  expect(createQs(qsObj0)).toBe(
-    "?page=2&pageSize=10&total=205&somethingElse=value"
-  );
-  expect(createQs(qsObj1)).toBe(
-    "?page=11&pageSize=12&total=350&somethingElse=anotherValue"
-  );
-  expect(createQs(qsObj2)).toBe("?page=99");
+  test("short object", () => {
+    const qsObj2: QsObj = {
+      page: 99,
+    };
+    expect(createQs(qsObj2)).toBe("?page=99");
+  });
 });
 
 // Task 3
-test("parseQs test", () => {
-  const qs0 = "?page=2&pageSize=10&total=205&somethingElse=value";
-  const qs1 = "?page=11&pageSize=12&total=350";
-  const qs2 = "?page=99";
+describe("parseQs test", () => {
+  test("long string", () => {
+    const qs0 = "?page=2&pageSize=10&total=205&somethingElse=value";
+    expect(parseQs(qs0)).toMatchObject({
+      page: "2",
+      pageSize: "10",
+      total: "205",
+      somethingElse: "value",
+    });
+  });
 
-  expect(parseQs(qs0)).toMatchObject({
-    page: "2",
-    pageSize: "10",
-    total: "205",
-    somethingElse: "value",
+  test("medium string", () => {
+    const qs1 = "?page=11&pageSize=12&total=350";
+    expect(parseQs(qs1)).toMatchObject({
+      page: "11",
+      pageSize: "12",
+      total: "350",
+    });
   });
-  expect(parseQs(qs1)).toMatchObject({
-    page: "11",
-    pageSize: "12",
-    total: "350",
-  });
-  expect(parseQs(qs2)).toMatchObject({
-    page: "99",
+
+  test("short string", () => {
+    const qs2 = "?page=99";
+    expect(parseQs(qs2)).toMatchObject({
+      page: "99",
+    });
   });
 });
--- src/functional-programming/ramdaPureFunctions.test.ts
+++ src/functional-programming/ramdaPureFunctions.test.ts
@@ -6,37 +6,43 @@ import {
   parseQs,
 } from "./ramdaPureFunctions";
 
-test("getTopName", () => {
-  const teams: Team[] = [
-    { name: "Lions", score: 5 },
-    { name: "Tigers", score: 4 },
-    { name: "Bears", score: 6 },
-    { name: "Monkeys", score: 2 },
-  ];
+describe("getTopName", () => {
+  test("common object", () => {
+    const teams: Team[] = [
+      { name: "Lions", score: 5 },
+      { name: "Tigers", score: 4 },
+      { name: "Bears", score: 6 },
+      { name: "Monkeys", score: 2 },
+    ];
 
-  expect(getTopName(teams)).toBe("Bears");
+    expect(getTopName(teams)).toBe("Bears");
+  });
 });
 
-test("createQs", () => {
-  const qsObj: QsObj = {
-    page: "2",
-    pageSize: "10",
-    total: "205",
-    somethingElse: "value",
-  };
+describe("createQs", () => {
+  test("common object", () => {
+    const qsObj: QsObj = {
+      page: "2",
+      pageSize: "10",
+      total: "205",
+      somethingElse: "value",
+    };
 
-  expect(createQs(qsObj)).toBe(
-    "?page=2&pageSize=10&total=205&somethingElse=value"
-  );
+    expect(createQs(qsObj)).toBe(
+      "?page=2&pageSize=10&total=205&somethingElse=value"
+    );
+  });
 });
 
-test("parseQs", () => {
-  const qs = "?page=2&pageSize=10&total=205&somethingElse=value";
+describe("parseQs", () => {
+  test("common string", () => {
+    const qs = "?page=2&pageSize=10&total=205&somethingElse=value";
 
-  expect(parseQs(qs)).toMatchObject({
-    page: "2",
-    pageSize: "10",
-    total: "205",
-    somethingElse: "value",
+    expect(parseQs(qs)).toMatchObject({
+      page: "2",
+      pageSize: "10",
+      total: "205",
+      somethingElse: "value",
+    });
   });
 });
